446c8f8585b9de131df037be54495af854ff7ea8
CalebH-dev <getitright109@gmail.com>
2025-09-04 09:19:19 -0400

    my second comit

--- chapters/03_1_lesson/exercises.py
+++ chapters/03_1_lesson/exercises.py
@@ -1,12 +1,32 @@
 
 ##### Template for Chapter 3.14, Exercises 1 - 3 ######
 
-
-print("********** Ch 3 Exercise 1 **********")
-
 # Do your work for Exercise 1 here
 
-print("Ch 3 Exercise 1: Not implemented") # Delete this line when you write your code!
+print("********** Ch 3 Exercise 1 **********")
+print()
+print()
+
+
+var1 = "hi"
+var2 = "hello"
+var3 = "fhadhbvidfbwjevhfdbfjfv"
+
+def right_justify(input):
+
+    buffer = ""
+    counter = 0
+    while counter < (64 - len(input)):
+        buffer+=" "
+        counter+= 1
+    buffer+=input
+    print(buffer)
+
+right_justify(var1)
+right_justify(var2)
+right_justify(var3)
+
+
 
 
 
